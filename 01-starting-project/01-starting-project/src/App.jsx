66beb37be69bb6669ef0b69d4f419547dfd8d9f3
racxak <92050973+racxak@users.noreply.github.com>
2023-12-15 14:57:09 +0100

    using props

--- 01-starting-project/01-starting-project/src/App.jsx
+++ 01-starting-project/01-starting-project/src/App.jsx
@@ -1,34 +1,56 @@
-import ReactImg from './assets/react-core-concepts.png';
+import reactImg from "./assets/react-core-concepts.png";
+import componentsImg from "./assets/Components.png";
 
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
+	return Math.floor(Math.random() * (max + 1));
 }
 
+function CoreConcept(props) {
+	return (
+		<li>
+			<img src={props.image} alt={props.title} />
+			<h3>{props.title}</h3>
+			<p>{props.description}</p>
+		</li>
+	);
+}
 function Header() {
-  const description = reactDescriptions[genRandomInt(reactDescriptions.length)]
-  return(
-<header>
-        <img src={ReactImg} alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          {description} React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-  )
+	const description = reactDescriptions[genRandomInt(reactDescriptions.length)];
+	return (
+		<header>
+			<img src={reactImg} alt="Stylized atom" />
+			<h1>React Essentials</h1>
+			<p>
+				{description} React concepts you will need for almost any app you are
+				going to build!
+			</p>
+		</header>
+	);
 }
 
 function App() {
-  return (
-    <div>
-      <Header/>
-      <main>
-        <h2>Time to get started!</h2>
-      </main>
-    </div>
-  );
+	return (
+		<div>
+			<Header />
+			<main>
+				<section id="core-concepts">
+					<h2>Core Concepts</h2>
+					<ul>
+						<CoreConcept
+							title="Components"
+							description="The core UI building block."
+							image={componentsImg}
+						/>
+						<CoreConcept />
+						<CoreConcept />
+						<CoreConcept />
+					</ul>
+				</section>
+			</main>
+		</div>
+	);
 }
 
 export default App;
