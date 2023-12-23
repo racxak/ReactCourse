893576d7799e47fd041291d0fa3b08a2415889d6
racxak <92050973+racxak@users.noreply.github.com>
2023-12-23 08:56:29 +0100

    task10

---
+++ 01-starting-project/01-starting-project/src/tasks/Taks10.jsx
@@ -0,0 +1,29 @@
+// You're working on a "Todo List" web app and your task is to output a list of dummy todo items dynamically. For this task, a Todo component has been prepared for you, though you must still add some code to it to receive and output the todo text.
+
+// To be more precise: In the App component, you should transform the DUMMY_TODOS array that's provided to you (which must not be changed!) to a list of JSX elements (<Todo> elements to be precise). Every Todo component item must receive and output the todo text via a prop called text.
+
+import React from 'react';
+
+import Todo from './Todo'
+
+
+// don't remove the export keyword here!
+export const DUMMY_TODOS = [
+    'Learn React',
+    'Practice React',
+    'Profit!'
+];
+
+// don't change the Component name "App"
+export default function App() {
+return(     
+    DUMMY_TODOS.map((item)=><Todo text={item} /> )
+  
+  )
+}
+
+import React from 'react';
+
+export default function Todo({text}) {
+    return <li>{text}</li>;
+}
