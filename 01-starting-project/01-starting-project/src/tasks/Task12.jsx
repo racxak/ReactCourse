d42abb626000b70ec0677122fe5b7975dc65ed5a
racxak <92050973+racxak@users.noreply.github.com>
2023-12-23 22:48:53 +0100

    task12

---
+++ 01-starting-project/01-starting-project/src/tasks/Task12.jsx
@@ -0,0 +1,16 @@
+// Your task is to work on the Input component such that it either returns a <textarea> element or an <input> element, depending on whether a richText prop set on Input is true or false.
+
+// I.e., if used like this:
+
+// <Input richText />
+// the Input component should render a <textarea>. Otherwise, it should render an <input>.
+
+// In addition, the Input component should forward all other props directly to the returned <textarea> or <input> elements.
+
+export default function Input({ richText, ...props }) {
+	return (
+		<>
+			{richText ? <textarea {...props}></textarea> : <input {...props}></input>}
+		</>
+	);
+}
