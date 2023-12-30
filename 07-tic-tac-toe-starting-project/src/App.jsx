aca99dea9141b03370894722f795e76189ea8639
racxak <92050973+racxak@users.noreply.github.com>
2023-12-30 22:59:19 +0100

    XO change - lifting state up

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,17 +1,23 @@
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
+import { useState } from "react";
 
 function App() {
+	const [activePlayer, setActivePlayer] = useState("X");
+
+	function handleSelectedSquare(){
+		setActivePlayer((curActivePlayer) => curActivePlayer ==="X" ? "O" : "X")
+	}
 	return (
 		<main>
 			<div id="game-container">
-				<ol id="players">
+				<ol id="players" className="highlight-player">
 					
+					<Player initialName= "Player 1" symbol="X" isActive={activePlayer==="X"}/>
+					<Player initialName= "Player 2" symbol="O" isActive={activePlayer==="O"}/>
 
-					<Player initialName= "Player 1" symbol="X"/>
-					<Player initialName= "Player 2" symbol="O"/>
 				</ol>
-				<GameBoard/>
+				<GameBoard  activeSymbol={activePlayer} onSelectSquare = {handleSelectedSquare}/>
 			</div>
 			Log
 		</main>
