1478b37dc6fd56f009e28bbcd3c2c0e8da8bfbeb
racxak <92050973+racxak@users.noreply.github.com>
2024-01-01 18:33:21 +0100

    winner detection

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -3,6 +3,13 @@ import { useState } from "react";
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
+import { WINNING_COMBINATIONS } from "./winning-combinations";
+
+const initialGameBoard = [
+	[null, null, null],
+	[null, null, null],
+	[null, null, null],
+];
 
 function deriveActivePlayer(gameTurns){
 	let curPlayer = "X";
@@ -15,9 +22,26 @@ function deriveActivePlayer(gameTurns){
 function App() {
 	// const [activePlayer, setActivePlayer] = useState("X");
 	const [gameTurns, setGameTurns] = useState([]);
-
 	const activePlayer = deriveActivePlayer(gameTurns);
 			
+	let gameBoard = initialGameBoard;
+	let winner;
+
+  for (const turn of gameTurns){
+   const {square, player} = turn;
+   const {row, col} = square; 
+   gameBoard[row][col]=player;
+  }
+
+	for (const combination of WINNING_COMBINATIONS){
+		const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+		const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
+		const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
+		if(firstSquareSymbol && firstSquareSymbol===secondSquareSymbol && secondSquareSymbol === thirdSquareSymbol){
+			winner = firstSquareSymbol;
+		}
+	}
+
 	function handleSelectedSquare(rowIndex, colIndex) {
 		// setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
 		setGameTurns((prevTurns) => {
@@ -46,7 +70,8 @@ function App() {
 						isActive={activePlayer === "O"}
 					/>
 				</ol>
-				<GameBoard turns={gameTurns} onSelectSquare={handleSelectedSquare} />
+				{winner && <p> you won, {winner}!!!</p>}
+				<GameBoard board={gameBoard} onSelectSquare={handleSelectedSquare} />
 			</div>
 			<Log turns={gameTurns} />
 		</main>
