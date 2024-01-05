f0e05b8921ed9ea6b32262cae5def6fa5b16cc27
racxak <92050973+racxak@users.noreply.github.com>
2024-01-05 13:38:19 +0100

    styling with tailwind css

--- 03-starting-project/src/components/AuthInputs.jsx
+++ 03-starting-project/src/components/AuthInputs.jsx
@@ -3,12 +3,12 @@ import {styled } from 'styled-components';
 import Button from './Button';
 import Input from './Input';
 
-const ControlContainer = styled.div`
-display: flex;
-flex-direction: column;
-gap: 0.5rem;
-margin-bottom: 1.5rem;
-`
+// const ControlContainer = styled.div`
+// display: flex;
+// flex-direction: column;
+// gap: 0.5rem;
+// margin-bottom: 1.5rem;
+// `
 
 
 export default function AuthInputs() {
@@ -32,8 +32,8 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <ControlContainer>
+    <div id="auth-inputs" className='w-full max-w-sm p-8 rounded show-md bg-gradient-to-b from-stone-700 to-stone-800 mx-auto'>
+      <div className='flex flex-col gap-2 mb-6 '>
          {/* <p>
          <Label $invalid={emailNotValid} >Email</Label>
            */}
@@ -60,12 +60,12 @@ export default function AuthInputs() {
             }
           />
         {/* </p> */}
-      </ControlContainer>
-      <div className="actions">
-        <button type="button" className="text-button">
+      </div>
+      <div className="flex justify-end gap-4 ">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
         </button>
-        <Button className='button' onClick={handleLogin}>Sign In</Button>
+        <Button onClick={handleLogin}>Sign In</Button>
       </div>
     </div>
   );
