55a758d1ec83bbab4e7ae9b0014140ce56fd46ad
racxak <92050973+racxak@users.noreply.github.com>
2024-01-07 21:08:37 +0100

    task3

--- 05-starting-project/src/components/ResultModal.jsx
+++ 05-starting-project/src/components/ResultModal.jsx
@@ -1,5 +1,4 @@
 import { forwardRef } from 'react';
-import {fowardRef} from 'react';
 
 const ResultModal=forwardRef( function ResultModal({result, targetTime},ref){
   return(<dialog ref={ref} className="result-modal" open> 
