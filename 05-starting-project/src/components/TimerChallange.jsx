b0e2026580080c2c77323f0117c8a2911836c5f8
racxak <92050973+racxak@users.noreply.github.com>
2024-01-07 14:13:19 +0100

    Using Refs

--- 05-starting-project/src/components/TimerChallange.jsx
+++ 05-starting-project/src/components/TimerChallange.jsx
@@ -1,27 +1,36 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
-export default function TimerChallange({title, targetTimer}){
-  const [challengeStarted, setChallengeStrted] = useState(false);
-  const [timerExpired, setTimerExpired] = useState(false);
+export default function TimerChallange({ title, targetTimer }) {
+	const timer = useRef();
+	const [challengeStarted, setChallengeStrted] = useState(false);
+	const [timerExpired, setTimerExpired] = useState(false);
 
-  function handleStart(){
-    setChallengeStrted(true)
-    const timer = setTimeout(()=>{
-      setTimerExpired(true)
-      setChallengeStrted(false);
-    }, targetTimer*1000)
-  }
-  
-  return(<section className="challenge"> 
-  <h2>{title}</h2>
-  {timerExpired && <p> You lost! </p>}
-  <p className="challenge-time">
-    {targetTimer} second{targetTimer>1 &&"s"}
-  </p>
-  <button onClick={handleStart}>
-    {challengeStarted ? "Stop" : "Start"} Challenge
-  </button>
-  <p className={challengeStarted? "active": undefined}>
-     {challengeStarted ? "Time is running..." : "Timer inactive"}</p>
-  </section>);
-};
+	function handleStart() {
+		setChallengeStrted(true);
+		timer.current = setTimeout(() => {
+			setTimerExpired(true);
+			setChallengeStrted(false);
+		}, targetTimer * 1000);
+	}
+
+	function handleStop(){
+		setChallengeStrted(false)
+		clearTimeout(timer.current)
+	}
+
+	return (
+		<section className="challenge">
+			<h2>{title}</h2>
+			{timerExpired && <p> You lost! </p>}
+			<p className="challenge-time">
+				{targetTimer} second{targetTimer > 1 && "s"}
+			</p>
+			<button onClick={ challengeStarted? handleStop: handleStart}>
+				{challengeStarted ? "Stop" : "Start"} Challenge
+			</button>
+			<p className={challengeStarted ? "active" : undefined}>
+				{challengeStarted ? "Time is running..." : "Timer inactive"}
+			</p>
+		</section>
+	);
+}
