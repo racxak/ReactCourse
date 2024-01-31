4954e5e0ebb736226105e26d685776d288584639
racxak <92050973+racxak@users.noreply.github.com>
2024-01-31 22:04:49 +0100

    modal

--- 06-starting-project/src/components/Form.jsx
+++ 06-starting-project/src/components/Form.jsx
@@ -1,6 +1,9 @@
 import { useRef } from "react";
 import Input from "./Input";
+import Modal from "./Modal";
+
 export default function Form({handleSaveProject}) {
+	const modal = useRef();
 	const title = useRef();
 	const description = useRef();
 	const dueDate = useRef();
@@ -9,6 +12,12 @@ export default function Form({handleSaveProject}) {
 		const enteredTitle = title.current.value;
 		const enteredDes = description.current.value;
 		const enteredDueDate = dueDate.current.value;
+
+		if(enteredTitle.trim() === '' || enteredDes.trim() === '' || enteredDueDate.trim() === ''){
+			modal.current.open();
+			return;
+		} 
+
 		handleSaveProject({
 			title: enteredTitle,
 			description: enteredDes,
@@ -18,12 +27,18 @@ export default function Form({handleSaveProject}) {
 	}
 
 	return (
+		<>
+		<Modal  ref={modal} buttonCaption="Save"> 
+			<h2 className='my-4 text-stone-700 text-xl font-bold' >Incalid Input</h2>
+			<p className='text-stone-600 mb-4'>Opps... looks like you forgot to eneter a value.</p>
+			<p className='text-stone-600 mb-4'>Please make sure you provide a valid value for every input field </p>
+		</Modal>
 		<div className="w-[35rem] mt-16 ">
 			<menu className="flex items-center justify-end gap-4 my-4">
 				<li>
-					<buttonc className="text-stone-800 hover:text-stone-950">
+					<button className="text-stone-800 hover:text-stone-950">
 						Cancel
-					</buttonc>
+					</button>
 				</li>
 				<li>
 					<button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"	
@@ -41,5 +56,6 @@ export default function Form({handleSaveProject}) {
 
 			</div>
 		</div>
+		</>
 	);
 }
